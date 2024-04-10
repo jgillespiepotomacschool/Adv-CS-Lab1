0307bd89b0d3bc0042f22c01957977e9b2ca4c47
jgillespiepotomacschool <jgillespie@potomacschool.org>
2024-04-10 10:40:01 -0400

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,11 @@
+
+/**
+*  Lab 1 Instructions - https://docs.google.com/document/d/1O71sNBJp7EFsDqqoILkEJTUr-lCn_7d2/edit?usp=sharing&ouid=117188448939699827035&rtpof=true&sd=true
+*/
+import java.util.Scanner;
+
+public class Lab1{
+  public static void main(){
+    
+  }
+}
