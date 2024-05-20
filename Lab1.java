c96b813f9baece0851b59484bed4273869aeecfe
jgillespiepotomacschool <jgillespie@potomacschool.org>
2024-05-20 13:31:56 -0400

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -4,8 +4,70 @@
 */
 import java.util.Scanner;
 
-public class Lab1{
-  public static void main(){
+public class Main { 
+    public static void main(String args[]) {
+        // Call all your methods from the main - test each method at least three times
+        // Don't assume it works correctly since it gives you an answer, make sure the returned value is correct
+        // For example
+        
+        int a = forSum( 5 );
+        System.out.println("forSum 5 = " + a);
+        System.out.println("forSum 10 = " + forSum(10));  
+        System.out.println("forSum 13 = " + forSum(13));  
+
+        // write the other method calls, testing each method three times
+        
+    }
+
     
-  }
+    /**
+    * This method finds the sum of the numbers 0 to x
+    *
+    * @param x Any whole number
+    * @return The sum of 0 to x
+    */
+    public static int forSum(int x) {
+
+        
+        return 0;
+    }
+    
+    /**
+    * This method will find the result of x raised to the power of y
+    *
+    * @param x Any whole number
+    * @param y Any whole number
+    * @return result of x raised to the power of y
+    */
+    public static double power(int x, int y) {
+
+        
+        return 0;
+    }
+    
+    /**
+    * This will determine if all the individual digits of x divide evenly into x
+    *
+    * @param x A positive whole number
+    * @return True if x is a self divisor, false otherwise
+    */
+    public static boolean selfDivisor(int x) {
+
+        
+        return false;
+    }
+    
+    
+    /**
+    * This method will find the xth fibonacci number
+    * This is the extended challenge
+    *
+    * @param x A positive whole number
+    * @return the xth fibonacci number
+    */
+    public static int findFibonacci(int x) {
+
+        
+        return 0;
+    }
 }
